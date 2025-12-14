c7b7e257836b5f598e2ae4ef1ee63eace8ae0a87
J Navaneetha Krishnaa <163708616+NavaneethJ03@users.noreply.github.com>
2025-12-14 09:58:28 +0530

    Add Fibonacci number calculation methods
    Implemented three methods to calculate Fibonacci numbers: iterative, top-down tabulation, and bottom-up memoization.

---
+++ DP/1 . (509) Fibonacci Number.py
@@ -0,0 +1,44 @@
+class Solution:
+    def fib(self , n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        f1 = 0 
+        f2 = 1
+        summ = f1 + f2
+        for i in range(2 , n+1):
+            summ = f1 + f2 
+            f1 = f2 
+            f2 = summ 
+
+        return summ 
+
+# Time Complexity - O(N) 
+# Space Complexity - O(1) 
+
+# Top - Down Tabulation
+class Solution:
+    def fib(self , n:int) -> int:
+        dp = [n+1] * 0
+        dp[0] , dp[1] = 0 , 1
+        for i in range(2 , n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n-1]
+# Time Complexity - O(N)
+# Space Complexity - O(N)
+
+# Bottom - Up Memoization 
+class Solution:
+    def fib(self , n: int) -> int:
+        memo = {0:0 , 1:1}
+        def f(x):
+            if x in memo:
+                return memo[x]
+            else:
+                memo[x] = f(x-1) + f(x-2)
+                return memo[x]
+        return f(n)
+
+# Time Complexity - O(N)  
+# Space Complexity - O(N)
